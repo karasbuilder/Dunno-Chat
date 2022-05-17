f969438f0ab89f7dac732d9b896c5d606418bdb0
Kafierence <vannam34760@gmail.com>
2022-05-17 10:08:06 +0700

    [Fix] fix deploy User display from firebase

--- app/src/main/java/ChatApp/android/Activities/UserHomeChat.java
+++ app/src/main/java/ChatApp/android/Activities/UserHomeChat.java
@@ -1,12 +1,20 @@
 package ChatApp.android.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -19,9 +27,11 @@ import ChatApp.android.databinding.ActivityUserHomeChatBinding;
 public class UserHomeChat extends AppCompatActivity {
     private ActivityUserHomeChatBinding binding;
     FirebaseDatabase database;
-    ArrayList<User> user;
-    UserAdapter userAdapter;
+    ArrayList<User> users;
+    UserAdapter usersAdapter;
     ProgressDialog dialog;
+
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,51 @@ public class UserHomeChat extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         //load to firebase get all the current user
+        database=FirebaseDatabase.getInstance();
+
+
+
+        users = new ArrayList<>();
+        database.getReference().child("users").child(FirebaseAuth.getInstance().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        user = snapshot.getValue(User.class);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+        usersAdapter = new UserAdapter(this, users);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        binding.recyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    User user = snapshot1.getValue(User.class);
+                    if(!user.getUid().equals(FirebaseAuth.getInstance().getUid()))
+                        users.add(user);
+
+                }
+
+                usersAdapter.notifyDataSetChanged();
+                System.out.println(usersAdapter.getItemCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
--- app/src/main/java/ChatApp/android/MainActivity.java
+++ app/src/main/java/ChatApp/android/MainActivity.java
@@ -9,15 +9,25 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 import ChatApp.android.Activities.PhoneNumberVerify;
+import ChatApp.android.Activities.UserHomeChat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView btnSignUp;
+    TextView btnSignUp,btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+
+        btnSignIn=findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, UserHomeChat.class);
+                startActivity(intent);
+            }
+        });
         btnSignUp=findViewById(R.id.btnSignUp);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
