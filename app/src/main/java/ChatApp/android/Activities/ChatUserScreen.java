83c1c3ff4d9fc9228ee402b90a491b421baa8898
Kafierence <vannam34760@gmail.com>
2022-05-19 22:34:04 +0700

    [Update] update new feature load chat between 2 people

--- app/src/main/java/ChatApp/android/Activities/ChatUserScreen.java
+++ app/src/main/java/ChatApp/android/Activities/ChatUserScreen.java
@@ -1,18 +1,119 @@
 package ChatApp.android.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import ChatApp.android.Adapters.MessageAdapter;
+import ChatApp.android.Model.Message;
 import ChatApp.android.databinding.ActivityChatUserScreenBinding;
 
 public class ChatUserScreen extends AppCompatActivity {
     ActivityChatUserScreenBinding binding;
+    //call to database get the storage
+    MessageAdapter adapter;
+    ArrayList<Message> messages;
+
+    String senderRoom, receiverRoom;
+
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+
+    ProgressDialog dialog;
+    String senderUid;
+    String receiverUid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        binding=ActivityChatUserScreenBinding.inflate(getLayoutInflater());
        setContentView(binding.getRoot());
+
+         setSupportActionBar(binding.toolbar);
+        database=FirebaseDatabase.getInstance();
+        storage=FirebaseStorage.getInstance();
+        //set
+        messages=new ArrayList<>();
+
+        receiverUid = getIntent().getStringExtra("uid");
+        senderUid = FirebaseAuth.getInstance().getUid();
+
+        senderRoom=senderUid+receiverUid;
+        receiverRoom=receiverUid+senderUid;
+        adapter = new MessageAdapter(this, messages, senderRoom, receiverRoom);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(adapter);
+        //loading from chat in firebase
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .child("messages")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messages.clear();
+                        for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            Message message = snapshot1.getValue(Message.class);
+                            message.setMessageId(snapshot1.getKey());
+                            messages.add(message);
+                        }
+
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageTxt=binding.messageBox.getText().toString();
+
+                //we will store date , timespan into chats
+                Date date=new Date();
+                Message message=new Message(messageTxt,senderUid,date.getTime());
+                binding.messageBox.setText("");
+                String randomKey = database.getReference().push().getKey();
+                //now store into database
+
+
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .child("messages")
+                        .child(randomKey)
+                        .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                database.getReference().child("chats")
+                                        .child(receiverRoom)
+                                        .child("messages")
+                                        .child(randomKey)
+                                        .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
 
     }
 }
